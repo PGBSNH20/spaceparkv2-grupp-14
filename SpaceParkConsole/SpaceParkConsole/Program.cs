9a808032e0d31c3a0d6e5b6608bb5564c79ab232
Kio <48633146+Kiomars93@users.noreply.github.com>
2021-05-01 15:29:46 +0200

    Small changes

--- SpaceParkConsole/SpaceParkConsole/Program.cs
+++ SpaceParkConsole/SpaceParkConsole/Program.cs
@@ -12,14 +12,20 @@ namespace SpaceParkConsole
             if(option.ToString() == logInChoice[0])
             {
                 Console.Clear();
+                Console.WriteLine("Register your username!");
+                var userInput = Console.ReadLine();
+                Console.WriteLine("Register your password!");
+                var passInput = Console.ReadLine();
+                //Docker inkludering för att spara ner det i DB:n
+                Console.WriteLine("Done with the registration!");
+            }
+            else
+            {
+                //Docker inkludering här med. Behöver manuellt lägga in ett admin konto.
                 Console.WriteLine("Enter your username!");
                 var userInput = Console.ReadLine();
                 Console.WriteLine("Enter your password!");
                 var passInput = Console.ReadLine();
-            }
-            else
-            {
-                Console.WriteLine("Hello");
             }
         }
     }
