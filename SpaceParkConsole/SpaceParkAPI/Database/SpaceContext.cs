bcad64cf774b5f49e8edf18cfda2499907666757
Kio <48633146+Kiomars93@users.noreply.github.com>
2021-05-03 15:31:28 +0200

    Add DBContext

---
+++ SpaceParkConsole/SpaceParkAPI/Database/SpaceContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpaceParkAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaceParkAPI.Database
+{
+    public class SpaceContext : DbContext
+    {
+        private const string connectionString = "Data source=LAPTOP-45O2J506;Database=ParkingSpaceVersion2;Trusted_Connection=True;";
+        public DbSet<People> Peoples { get; set; }
+        public SpaceContext() : base()
+        {
+
+        }
+        //public override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    if (optionsBuilder.IsConfigured == false)
+        //        optionsBuilder.UseSqlServer(connectionString);
+        //    //optionsBuilder.UseSqlServer(connectionString);
+        //}
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+
+}
--- SpaceParkConsole/SpaceParkConsole/Program.cs
+++ SpaceParkConsole/SpaceParkConsole/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using RestSharp;
+using System;
+using System.Net.Http;
+using System.Text.Json;
 
 namespace SpaceParkConsole
 {
@@ -7,14 +10,30 @@ namespace SpaceParkConsole
         static void Main(string[] args)
         {
             var logInChoice = new string[] { "Register", "Log in" };
+            var parkingChoice = new string[] { "Register Parking", "Pay for parking", "exit" };
             var option = Menu.ShowMenu("Welcome", logInChoice);
 
-            if(option.ToString() == logInChoice[0])
+            if (option.ToString() == logInChoice[0])
             {
                 var username = Menu.RequestUserInput("Register your username!");
                 var password = Menu.RequestUserInput("Register your password!");
+                //Password Encryption
                 //Docker inkludering för att spara ner det i DB:n
                 Console.WriteLine("Registration complete!");
+                Console.ReadLine();
+                var parkOption = Menu.ShowMenu("Welcome", parkingChoice);
+                if (parkOption.ToString() == parkingChoice[0])
+                {
+
+                }
+                else if (parkOption.ToString() == parkingChoice[1])
+                {
+
+                }
+                else if (parkOption.ToString() == parkingChoice[2])
+                {
+                    Console.WriteLine("Thank you, goodbye");
+                }
             }
             else
             {
