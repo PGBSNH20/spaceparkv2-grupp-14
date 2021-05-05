260d1128ba6850737e0652a0c34de25dac666132
SPEEDYGONZALES\Sebastian <s.ebrandt@hotmail.com>
2021-05-05 22:43:30 +0200

    tests for password encryption

---
+++ SpaceParkConsole/SpaceParkAPITests/SecurityTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SpaceParkAPI.Security;
+using Xunit;
+
+namespace SpaceParkAPITests
+{
+    public class SecurityTests
+    {
+        [Fact]
+        public void EnryptPassword_Test()
+        {
+            string pw = "test123";
+            string encrypted = Encryption.Encrypt(pw);
+
+            Assert.Equal("{lz{89:", encrypted);
+        }
+    }
+}
