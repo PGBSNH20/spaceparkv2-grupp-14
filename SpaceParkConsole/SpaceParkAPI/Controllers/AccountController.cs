e6b6756153fe241b0e8a21790740032ef5dd2c6f
SPEEDYGONZALES\Sebastian <s.ebrandt@hotmail.com>
2021-05-05 22:43:09 +0200

    GET and POST requests functionality added

--- SpaceParkConsole/SpaceParkAPI/Controllers/AccountController.cs
+++ SpaceParkConsole/SpaceParkAPI/Controllers/AccountController.cs
@@ -5,6 +5,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using SpaceParkAPI.Security;
+using SpaceParkAPI.Swapi;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -14,52 +18,70 @@ namespace SpaceParkAPI.Controllers
     [ApiController]
     public class AccountController : ControllerBase
     {
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
         // GET api/<AccountController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        [HttpGet("{username},{password}")]
+        public async Task<HttpResponseMessage> Get(string username, string password)
         {
-            return "value";
+            HttpResponseMessage response = new HttpResponseMessage();
+            using (var db = new SpaceContext())
+            {
+                try
+                {
+                    var acc = db.Accounts.First(x => x.Username.ToLower() == username.ToLower()
+                        && Encryption.Encrypt(password) == x.Password);
+
+                    response.StatusCode = HttpStatusCode.OK;
+                    response.ReasonPhrase = "Log in success.";
+                }
+                catch (InvalidOperationException) 
+                {
+                    response.StatusCode = HttpStatusCode.Unauthorized;
+                    response.ReasonPhrase = "Invalid login parameters.";
+                }
+            }
+            return response;
         }
 
         // POST api/<AccountController>
         [HttpPost]
-        public async void Post([FromBody] Account value)
+        public async Task<HttpResponseMessage> Post([FromBody] Account value)
         {
-            var ppl = new Swapi.SwapiPeople();
+            var ppl = new SwapiPeople();
             var result = await ppl.FetchAll();
-            var person =  result.Find(x => x.Name == value.People.Name);
+            var person = result.Find(x => x.Name == value.People.Name);
+
             using (var db = new SpaceContext())
             {
-                db.Accounts.Add(value);
-                db.SaveChanges();
-            }
-            if (person != null)
-            {
-                //lägg in datan
-                //return StatusCode.200_OK;
-            }
-            else
-            {
-                //return StatusCode.404_NotFound;
-            }
-        }
+                try
+                {
+                    Account acc = db.Accounts.First(x => x.Username == value.Username);
 
-        // PUT api/<AccountController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+                    return new HttpResponseMessage()
+                    {
+                        StatusCode = HttpStatusCode.Conflict,
+                        ReasonPhrase = $"Account with username {value.Username} already exists!"
+                    };
+                }
+                catch(InvalidOperationException)
+                {
+                }
 
-        // DELETE api/<AccountController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+                if (person != null)
+                {
+                    value.People.PersonalID = result.IndexOf(person) + 1;
+                    value.Password = Encryption.Encrypt(value.Password);
+                    db.Accounts.Add(value);
+                    db.SaveChanges();
+                    return new HttpResponseMessage(HttpStatusCode.Created);
+
+                }
+                else
+                {
+                    HttpResponseMessage msg = new HttpResponseMessage(HttpStatusCode.NotAcceptable);
+                    msg.ReasonPhrase = $"{value.People.Name} is not a valid Star Wars character, see https://swapi.dev/api/people/ for all valid characters.";
+                    return msg;
+                }
+            }
         }
     }
 }
